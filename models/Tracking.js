1fad8c288e4032fdf0cf48ba9ccb5425b00665cc
NoorAlJirdabi <noor.aljirdabi@gmail.com>
2024-10-01 10:43:40 +0300

    test done

--- models/Tracking.js
+++ models/Tracking.js
@@ -1,25 +1,20 @@
-
-const mongoose = require('mongoose');
-const {userSchema} = require('./User');
-const {exerciseSchema} = require('./Exercise');
+const mongoose = require('mongoose')
+const { userSchema } = require('./User')
+const { exerciseSchema } = require('./Exercise')
 
 const trackingSchema = mongoose.Schema(
-    {
-        duration: Number,
-        distance:Number,
-        calories_burned:Number,
-        date:Date,
-        // user: [userSchema],
-        // exercise:[exerciseSchema],
-        user:{ type: mongoose.Schema.Types.ObjectId },
-        exercise:{ type: mongoose.Schema.Types.ObjectId },
-
-    
-
-    },
-    {
-      timestamps: true //means createdAt and updatedAt
-    }
-
+  {
+    duration: Number,
+    distance: Number,
+    calories_burned: Number,
+    date: Date,
+    // user: [userSchema],
+    // exercise:[exerciseSchema],
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    exercise: { type: mongoose.Schema.Types.ObjectId, ref: 'Exercise' }
+  },
+  {
+    timestamps: true //means createdAt and updatedAt
+  }
 )
-module.exports = mongoose.model('Tracking',trackingSchema);
+module.exports = mongoose.model('Tracking', trackingSchema)
--- models/User.js
+++ models/User.js
@@ -1,4 +1,3 @@
-
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema(
@@ -7,10 +6,15 @@ const userSchema = mongoose.Schema(
     email: String,
     username: String,
     password: String,
-    age: Number,
-    gender: String,
+    age: {
+      type: Number,
+      default: null
+    },
+    gender: {
+      type: String,
+      default: null
+    },
     profile_picture: String,
-    // role:  String,
     role: { type: String, enum: ['user', 'admin'], default: 'user' },
     event: [
       {
@@ -28,36 +32,12 @@ const userSchema = mongoose.Schema(
     googleId: {
       type: String,
       required: true
-=======
-const mongoose = require('mongoose');
-
-const userSchema = mongoose.Schema(
-    {
-        name: String,
-        email: String,
-        username: String,
-        password: String,
-        age:{
-            type: Number, default: null
-        },
-        gender:{
-            type: String, default: null
-        },
-        profile_picture: String,
-        role:  String,
-        event:[{            //poplute('event')
-        type: mongoose.Schema.Types.ObjectId,
-        ref :'Event' 
-        }],
-        googleId:{
-            type:String,
-            required:true
-        }
-
-    },
-    {
-      timestamps: true //means createdAt and updatedAt
-
+    }
+  },
+  {
+    timestamps: true //means createdAt and updatedAt
+  }
+)
 
 const User = mongoose.model('User', userSchema)
 module.exports = { User, userSchema }
